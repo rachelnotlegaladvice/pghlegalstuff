9585d89ec43fece1bf5c9614993c3927d6f6859e
Richard Schulte <richard.j.schulte@gmail.com>
2015-07-18 23:41:12 -0400

    a really good start!

---
+++ js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$(document).ready(function(){
+     $(".section-item").height($(window).height());
+         $(window).resize(function(){
+             $(".section-item").height($(window).height());
+     });
+     $('.navbar-collapse .nav a').on('click', function(){
+        $(".navbar-collapse").collapse('hide'); //bootstrap 3.x by Richard
+      });
+      $('a[href^="#"]').on('click',function (e) {
+  	    e.preventDefault();
+
+  	    var target = this.hash;
+  	    var $target = $(target);
+
+  	    $('html, body').stop().animate({
+  	        'scrollTop': $target.offset().top
+  	    }, 900, 'swing', function () {
+  	        window.location.hash = target;
+  	    });
+  	});
+ });
