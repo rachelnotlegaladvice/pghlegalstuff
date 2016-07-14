bd9037aeebe4cc87a6f415a0fe7968f80b6fe98e
Richard Schulte <richard.j.schulte@gmail.com>
2016-07-14 01:44:25 -0400

    open external links in new window

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 'use strict';
 
 $(document).ready(function(){
+    /**
+     * Ensure each section is minimally the viewport height
+     */
      $(".section-item").css("min-height", $(window).height());
          $(window).resize(function(){
              $(".section-item").css("min-height", $(window).height());
      });
+     /**
+      * Collapse navbar when selecting an option
+      */
      $('.navbar-collapse .nav a').on('click', function(){
         $(".navbar-collapse").collapse('hide'); //bootstrap 3.x by Richard
       });
+      /**
+       * Animate anchor links
+       */
       $('a[href^="#"]').on('click',function (e) {
   	    e.preventDefault();
 
@@ -19,5 +28,14 @@ $(document).ready(function(){
   	    }, 900, 'swing', function () {
   	        window.location.hash = target;
   	    });
+  	});
+    /**
+     * Open all external links in new window
+     */
+    $('a').not('[href*="mailto:"]').each(function () {
+  		var isInternalLink = new RegExp('/' + window.location.host + '/');
+  		if ( ! isInternalLink.test(this.href) ) {
+  			$(this).attr('target', '_blank');
+  		}
   	});
  });
